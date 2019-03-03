b34337dc9ab6751192ac4298337db215e0a4617e
Stanley Nwakamma <44230618+stanleynwakamma@users.noreply.github.com>
2019-03-03 15:25:29 -0500

    Used the getMessageshelper function

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -61,25 +61,7 @@ public class Datastore {
             .setFilter(new Query.FilterPredicate("recipient", FilterOperator.EQUAL, recipient))
             .addSort("timestamp", SortDirection.DESCENDING);
     PreparedQuery results = datastore.prepare(query);
-    for (Entity entity : results.asIterable()) {
-        try {
-          String idString = entity.getKey().getName();
-          UUID id = UUID.fromString(idString);
-          String user = (String) entity.getProperty("user");
-
-          String text = (String) entity.getProperty("text");
-          long timestamp = (long) entity.getProperty("timestamp");
-
-          Message message = new Message(id, user, text, timestamp, recipient);
-          messages.add(message);
-        } catch (Exception e) {
-          System.err.println("Error reading message.");
-          System.err.println(entity.toString());
-          e.printStackTrace();
-        }
-      }
-
-      return messages;
+    return getMessagesHelper(results);
   }
 
    /**
