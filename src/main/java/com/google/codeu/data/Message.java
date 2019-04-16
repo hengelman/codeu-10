9636979cb586bfc7ff0b5983d8d7e6aeec787739
hengel <hengel@umich.edu>
2019-04-16 17:14:13 -0400

    added property to determine if message is public

--- src/main/java/com/google/codeu/data/Message.java
+++ src/main/java/com/google/codeu/data/Message.java
@@ -28,6 +28,7 @@ public class Message {
   private String text;
   private String subject;
   private long timestamp;
+  private boolean isPublic;
 
   /**
    * Constructs a new {@link Message} posted by {@code user} with {@code text} content. Generates a
@@ -45,6 +46,12 @@ public class Message {
     this.timestamp = timestamp;
     this.recipient = recipient;
     this.subject = subject;
+
+    if (user.equals(recipient)) {
+      this.isPublic = true;
+    } else {
+      this.isPublic = false;
+    }
   }
 
   public UUID getId() {
@@ -68,6 +75,10 @@ public class Message {
   }
 
   public String getSubject() {
-    return subject; 
+    return subject;
+  }
+
+  public boolean getPublic() {
+    return isPublic;
   }
 }
